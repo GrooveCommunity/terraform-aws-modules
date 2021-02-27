27a671c220aff30d154b71f42d6e920054f33bf1
Victor França Lopes <victor.franca@upper.institue>
2021-02-27 16:44:00 -0300

    Add private subnet and nat gateway to vpc

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,6 +1,13 @@
 locals {
+
   public_subnets = { for public_subnet in var.public_subnets : "${var.name}-${public_subnet.zone}-${public_subnet.name}" => public_subnet }
   intra_subnets  = { for intra_subnet in var.intra_subnets : "${var.name}-${intra_subnet.zone}-${intra_subnet.name}" => intra_subnet }
+
+  nat_gateway_zones = {
+    for nat_gateway in var.nat_gateways : nat_gateway.zone => merge({
+      subnet_key = "${var.name}-${nat_gateway.zone}-${nat_gateway.subnet}"
+    }, nat_gateway)
+  }
 }
 
 ######
@@ -24,6 +31,35 @@ resource "aws_vpc" "this" {
   )
 }
 
+#######################
+# Default Network ACLs
+#######################
+resource "aws_default_network_acl" "this" {
+
+  default_network_acl_id = aws_vpc.this.default_network_acl_id
+
+  subnet_ids = setsubtract(
+    compact(flatten([
+      aws_subnet.public.*.id,
+      aws_subnet.private.*.id,
+      aws_subnet.intra.*.id,
+    ])),
+    compact(flatten([
+      aws_network_acl.public.*.subnet_ids,
+      aws_network_acl.private.*.subnet_ids,
+      aws_network_acl.intra.*.subnet_ids,
+    ]))
+  )
+
+  tags = merge(
+    {
+      "Name" = "${var.name}-vpc-acl"
+    },
+    var.tags
+  )
+
+}
+
 ################
 # Public subnet
 ################
@@ -39,7 +75,7 @@ resource "aws_subnet" "public" {
 
   tags = merge(
     {
-      "Name" = each.key
+      "Name" = "${each.key}-${each.value["zone"]}-public"
     },
     var.tags,
   )
@@ -96,6 +132,194 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public[0].id
 }
 
+########################
+# Public Network ACLs
+########################
+resource "aws_network_acl" "public" {
+  vpc_id     = aws_vpc.this.id
+  subnet_ids = aws_subnet.public.*.id
+
+  tags = merge(
+    {
+      "Name" = "${var.name}-public-acl"
+    },
+    var.tags,
+  )
+}
+
+resource "aws_network_acl_rule" "public_inbound" {
+  count = length(var.public_inbound_acl_rules)
+
+  network_acl_id = aws_network_acl.public.id
+
+  egress          = false
+  rule_number     = var.public_inbound_acl_rules[count.index]["rule_number"]
+  rule_action     = var.public_inbound_acl_rules[count.index]["rule_action"]
+  from_port       = lookup(var.public_inbound_acl_rules[count.index], "from_port", null)
+  to_port         = lookup(var.public_inbound_acl_rules[count.index], "to_port", null)
+  icmp_code       = lookup(var.public_inbound_acl_rules[count.index], "icmp_code", null)
+  icmp_type       = lookup(var.public_inbound_acl_rules[count.index], "icmp_type", null)
+  protocol        = var.public_inbound_acl_rules[count.index]["protocol"]
+  cidr_block      = lookup(var.public_inbound_acl_rules[count.index], "cidr_block", null)
+  ipv6_cidr_block = lookup(var.public_inbound_acl_rules[count.index], "ipv6_cidr_block", null)
+}
+
+resource "aws_network_acl_rule" "public_outbound" {
+  count = length(var.public_outbound_acl_rules)
+
+  network_acl_id = aws_network_acl.public.id
+
+  egress          = true
+  rule_number     = var.public_outbound_acl_rules[count.index]["rule_number"]
+  rule_action     = var.public_outbound_acl_rules[count.index]["rule_action"]
+  from_port       = lookup(var.public_outbound_acl_rules[count.index], "from_port", null)
+  to_port         = lookup(var.public_outbound_acl_rules[count.index], "to_port", null)
+  icmp_code       = lookup(var.public_outbound_acl_rules[count.index], "icmp_code", null)
+  icmp_type       = lookup(var.public_outbound_acl_rules[count.index], "icmp_type", null)
+  protocol        = var.public_outbound_acl_rules[count.index]["protocol"]
+  cidr_block      = lookup(var.public_outbound_acl_rules[count.index], "cidr_block", null)
+  ipv6_cidr_block = lookup(var.public_outbound_acl_rules[count.index], "ipv6_cidr_block", null)
+}
+
+#####################################################
+# private subnets - private subnet with NAT gateway
+#####################################################
+resource "aws_subnet" "private" {
+  for_each = local.private_subnets
+
+  vpc_id                          = aws_vpc.this.id
+  cidr_block                      = each.value["cidr"]
+  availability_zone               = each.value["zone"]
+  map_public_ip_on_launch         = false
+  assign_ipv6_address_on_creation = false
+  ipv6_cidr_block                 = null
+
+  tags = merge(
+    {
+      "Name" = "${each.key}-${each.value["zone"]}-private"
+    },
+    var.tags,
+  )
+}
+
+#################
+# Private routes
+# There are as many routing tables as the number of NAT gateways
+#################
+resource "aws_route_table" "private" {
+  for_each = local.nat_gateway_zones
+
+  vpc_id = local.vpc_id
+
+  tags = merge(
+    {
+      "Name" = "routes-${var.name}-${var.each.value}"
+    },
+    var.tags,
+  )
+}
+
+resource "aws_route_table_association" "private" {
+  for_each = local.private_subnets
+
+  subnet_id      = aws_subnet.private[each.key].id
+  route_table_id = aws_route_table.private[each.value["zone"]].id
+}
+
+resource "aws_route" "private_nat_gateway" {
+  for_each = local.nat_gateway_zones
+
+  route_table_id         = aws_route_table.private[each.value["zone"]].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.this[each.key].id
+
+  timeouts {
+    create = "5m"
+  }
+}
+
+
+##############
+# NAT Gateway
+##############
+
+resource "aws_eip" "nat" {
+  for_each = { for key, zone in local.nat_gateway_zones : key => zone... if lookup(zone, "eip_id", null) != null }
+
+  vpc = true
+
+  tags = merge(
+    {
+      "Name" = "nat-ip-${var.name}-${each.key}"
+    },
+    var.tags,
+  )
+}
+
+resource "aws_nat_gateway" "this" {
+  for_each = local.nat_gateway_zones
+
+  allocation_id = lookup(each.value, "eip_id", lookup(lookup(aws_eip.nat, each.key, {}), "id", null))
+  subnet_id     = aws_subnet.public[each.value["subnet_key"]].id
+
+  tags = merge(
+    {
+      "Name" = "nat-${var.name}-${each.key}"
+    },
+    var.tags,
+  )
+
+  depends_on = [aws_internet_gateway.this]
+}
+
+########################
+# Private Network ACLs
+########################
+resource "aws_network_acl" "private" {
+  vpc_id     = aws_vpc.this.id
+  subnet_ids = aws_subnet.private.*.id
+
+  tags = merge(
+    {
+      "Name" = "${var.name}-private-acl"
+    },
+    var.tags,
+  )
+}
+
+resource "aws_network_acl_rule" "private_inbound" {
+  count = length(var.private_inbound_acl_rules)
+
+  network_acl_id = aws_network_acl.private.id
+
+  egress          = false
+  rule_number     = var.private_inbound_acl_rules[count.index]["rule_number"]
+  rule_action     = var.private_inbound_acl_rules[count.index]["rule_action"]
+  from_port       = lookup(var.private_inbound_acl_rules[count.index], "from_port", null)
+  to_port         = lookup(var.private_inbound_acl_rules[count.index], "to_port", null)
+  icmp_code       = lookup(var.private_inbound_acl_rules[count.index], "icmp_code", null)
+  icmp_type       = lookup(var.private_inbound_acl_rules[count.index], "icmp_type", null)
+  protocol        = var.private_inbound_acl_rules[count.index]["protocol"]
+  cidr_block      = lookup(var.private_inbound_acl_rules[count.index], "cidr_block", null)
+  ipv6_cidr_block = lookup(var.private_inbound_acl_rules[count.index], "ipv6_cidr_block", null)
+}
+
+resource "aws_network_acl_rule" "private_outbound" {
+  count = length(var.private_outbound_acl_rules)
+
+  network_acl_id = aws_network_acl.private.id
+
+  egress          = true
+  rule_number     = var.private_outbound_acl_rules[count.index]["rule_number"]
+  rule_action     = var.private_outbound_acl_rules[count.index]["rule_action"]
+  from_port       = lookup(var.private_outbound_acl_rules[count.index], "from_port", null)
+  to_port         = lookup(var.private_outbound_acl_rules[count.index], "to_port", null)
+  icmp_code       = lookup(var.private_outbound_acl_rules[count.index], "icmp_code", null)
+  icmp_type       = lookup(var.private_outbound_acl_rules[count.index], "icmp_type", null)
+  protocol        = var.private_outbound_acl_rules[count.index]["protocol"]
+  cidr_block      = lookup(var.private_outbound_acl_rules[count.index], "cidr_block", null)
+  ipv6_cidr_block = lookup(var.private_outbound_acl_rules[count.index], "ipv6_cidr_block", null)
+}
 
 #####################################################
 # intra subnets - private subnet without NAT gateway
@@ -112,7 +336,7 @@ resource "aws_subnet" "intra" {
 
   tags = merge(
     {
-      "Name" = each.key
+      "Name" = "${each.key}-${each.value["zone"]}-intra"
     },
     var.tags,
   )
@@ -139,4 +363,53 @@ resource "aws_route_table_association" "intra" {
 
   subnet_id      = aws_subnet.intra[each.key].id
   route_table_id = aws_route_table.intra[0].id
+}
+
+########################
+# Intra Network ACLs
+########################
+resource "aws_network_acl" "intra" {
+  vpc_id     = aws_vpc.this.id
+  subnet_ids = aws_subnet.intra.*.id
+
+  tags = merge(
+    {
+      "Name" = "${var.name}-intra-acl"
+    },
+    var.tags,
+  )
+}
+
+resource "aws_network_acl_rule" "intra_inbound" {
+  count = length(var.intra_inbound_acl_rules)
+
+  network_acl_id = aws_network_acl.intra.id
+
+  egress          = false
+  rule_number     = var.intra_inbound_acl_rules[count.index]["rule_number"]
+  rule_action     = var.intra_inbound_acl_rules[count.index]["rule_action"]
+  from_port       = lookup(var.intra_inbound_acl_rules[count.index], "from_port", null)
+  to_port         = lookup(var.intra_inbound_acl_rules[count.index], "to_port", null)
+  icmp_code       = lookup(var.intra_inbound_acl_rules[count.index], "icmp_code", null)
+  icmp_type       = lookup(var.intra_inbound_acl_rules[count.index], "icmp_type", null)
+  protocol        = var.intra_inbound_acl_rules[count.index]["protocol"]
+  cidr_block      = lookup(var.intra_inbound_acl_rules[count.index], "cidr_block", null)
+  ipv6_cidr_block = lookup(var.intra_inbound_acl_rules[count.index], "ipv6_cidr_block", null)
+}
+
+resource "aws_network_acl_rule" "intra_outbound" {
+  count = length(var.intra_outbound_acl_rules)
+
+  network_acl_id = aws_network_acl.intra.id
+
+  egress          = true
+  rule_number     = var.intra_outbound_acl_rules[count.index]["rule_number"]
+  rule_action     = var.intra_outbound_acl_rules[count.index]["rule_action"]
+  from_port       = lookup(var.intra_outbound_acl_rules[count.index], "from_port", null)
+  to_port         = lookup(var.intra_outbound_acl_rules[count.index], "to_port", null)
+  icmp_code       = lookup(var.intra_outbound_acl_rules[count.index], "icmp_code", null)
+  icmp_type       = lookup(var.intra_outbound_acl_rules[count.index], "icmp_type", null)
+  protocol        = var.intra_outbound_acl_rules[count.index]["protocol"]
+  cidr_block      = lookup(var.intra_outbound_acl_rules[count.index], "cidr_block", null)
+  ipv6_cidr_block = lookup(var.intra_outbound_acl_rules[count.index], "ipv6_cidr_block", null)
 }
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -6,7 +6,7 @@ variable "name" {
 variable "cidr" {
   description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden"
   type        = string
-  default     = "0.0.0.0/0"
+  default     = "10.30.0.0/16"
 }
 
 variable "instance_tenancy" {
@@ -26,12 +26,12 @@ variable "public_subnets" {
   type        = list(any)
   default = [
     {
-      name = "pfsense"
+      name = "public"
       cidr = "10.30.0.0/24"
       zone = "us-east-1a"
     },
     {
-      name = "pfsense"
+      name = "public"
       cidr = "10.30.1.0/24"
       zone = "us-east-1f"
     },
@@ -51,6 +51,137 @@ variable "intra_subnets" {
       name = "intra"
       cidr = "10.30.3.0/24"
       zone = "us-east-1f"
+    },
+  ]
+}
+
+
+variable "private_subnets" {
+  description = "Private subnets specification"
+  type        = list(any)
+  default = [
+    {
+      name = "private"
+      cidr = "10.30.4.0/24"
+      zone = "us-east-1a"
+    },
+    {
+      name = "private"
+      cidr = "10.30.5.0/24"
+      zone = "us-east-1f"
+    },
+  ]
+}
+
+variable "nat_gateway" {
+  description = "Availability zones to deploy AWS NAT Gateway for private subnets"
+  type        = list(any)
+  default = [
+    {
+      subnet = "public"
+      zone   = "us-east-1a"
+      # eip_id = aws_eip.nat.id
+    },
+    {
+      subnet = "public"
+      zone   = "us-east-1f"
+    }
+  ]
+}
+
+variable "public_inbound_acl_rules" {
+  description = "Public subnets inbound network ACLs"
+  type        = list(map(string))
+
+  default = [
+    {
+      rule_number = 100
+      rule_action = "allow"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_block  = "0.0.0.0/0"
+    },
+  ]
+}
+
+variable "public_outbound_acl_rules" {
+  description = "Public subnets outbound network ACLs"
+  type        = list(map(string))
+
+  default = [
+    {
+      rule_number = 100
+      rule_action = "allow"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_block  = "0.0.0.0/0"
+    },
+  ]
+}
+
+
+variable "private_inbound_acl_rules" {
+  description = "Private subnets inbound network ACLs"
+  type        = list(map(string))
+
+  default = [
+    {
+      rule_number = 100
+      rule_action = "allow"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_block  = "0.0.0.0/0"
+    },
+  ]
+}
+
+variable "private_outbound_acl_rules" {
+  description = "Private subnets outbound network ACLs"
+  type        = list(map(string))
+
+  default = [
+    {
+      rule_number = 100
+      rule_action = "allow"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_block  = "0.0.0.0/0"
+    },
+  ]
+}
+
+variable "intra_inbound_acl_rules" {
+  description = "Intra subnets inbound network ACLs"
+  type        = list(map(string))
+
+  default = [
+    {
+      rule_number = 100
+      rule_action = "allow"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_block  = "0.0.0.0/0"
+    },
+  ]
+}
+
+variable "intra_outbound_acl_rules" {
+  description = "Intra subnets outbound network ACLs"
+  type        = list(map(string))
+
+  default = [
+    {
+      rule_number = 100
+      rule_action = "allow"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_block  = "0.0.0.0/0"
     },
   ]
 }
