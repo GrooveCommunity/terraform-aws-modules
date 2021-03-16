7596c28f091abab082d246db820e95215e087d98
Renato Martins <renatto.marttins@gmail.com>
2021-03-16 00:28:57 -0300

    [EKS] Add map_users variable to add aws auth support

--- eks/main.tf
+++ eks/main.tf
@@ -1,3 +1,17 @@
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "14.0.0"
@@ -15,6 +29,8 @@ module "eks" {
       launch_template_version = aws_launch_template.this[launch_template.name].default_version
     })
   }
+
+  map_users = var.map_users
 
 }
 
