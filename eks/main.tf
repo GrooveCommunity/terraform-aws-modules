098c7c4632f250aa9d0ee6862f5c503862d892d1
Victor França Lopes <victor.franca@upper.institue>
2021-03-21 00:15:20 -0300

    Comment provider

--- eks/main.tf
+++ eks/main.tf
@@ -1,16 +1,18 @@
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
-}
+# If you need to configure something standard to all clusters use the code below
+#
+# data "aws_eks_cluster" "cluster" {
+#   name = module.eks.cluster_id
+# }
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
-}
+# data "aws_eks_cluster_auth" "cluster" {
+#   name = module.eks.cluster_id
+# }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.cluster.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+#   token                  = data.aws_eks_cluster_auth.cluster.token
+# }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
